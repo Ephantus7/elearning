6861a3489edaf2c34073e88aa8fca858608c8930
Ephantus7 <ephantus640@gmail.com>
2023-06-24 17:21:57 +0300

    Added changes.

--- elearning/production.py
+++ elearning/production.py
@@ -138,7 +138,6 @@ DEFAULT_FILE_STORAGE = 'core.azure_storage.AzureMediaStorage'
 
 AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
 AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
-AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 
 # media urls
 MEDIA_URL = 'https://' + os.environ['AZURE_STORAGE_ACCOUNT'] + '.blob.core.windows.net/media/'

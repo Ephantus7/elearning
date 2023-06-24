05895abee53426592c2983f5a92e1a15ea83d106
Ephantus7 <62132661+Ephantus7@users.noreply.github.com>
2023-06-24 17:57:07 +0000

    Added changes.

--- elearning/production.py
+++ elearning/production.py
@@ -32,24 +32,6 @@ DATABASES = {
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
-    'elearning_app.apps.ElearningAppConfig',
-    'embed_video',
-    'django_messages',
-    'django.contrib.sites',
-    'quiz',
-    'axes',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -63,76 +45,6 @@ MIDDLEWARE = [
     'axes.middleware.AxesMiddleware',
 ]
 
-ROOT_URLCONF = 'elearning.urls'
-
-AUTHENTICATION_BACKENDS = [
-    'axes.backends.AxesBackend',
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'elearning.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'Africa/Nairobi'
-
-USE_I18N = True
-
-USE_TZ = False
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# custom user model
-AUTH_USER_MODEL = 'elearning_app.CustomUser'
-
 # azure storage setitngs
 DEFAULT_FILE_STORAGE = 'elearning.azure_storage.AzureMediaStorage'
 
@@ -140,26 +52,5 @@ AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
 AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
 
 # media urls
-MEDIA_URL = 'https://' + os.environ['AZURE_STORAGE_ACCOUNT'] + '.blob.core.windows.net/media/'
+MEDIA_URL = 'https://' + os.environ['AZURE_ACCOUNT_NAME'] + '.blob.core.windows.net/media'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-# login/logout urls
-LOGOUT_REDIRECT_URL = 'home'
-
-AXES_FAILURE_LIMIT: 5
-
-AXES_COOLOFF_TIME: 1
-
-AXES_RESET_ON_SUCCESS = True
-
-FORM_RENDERER = 'django.forms.renderers.DjangoTemplates'
-
-# allauth
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_AUTHENTICATION_METHOD = ("username_email")
-
-# custom message tags
-MESSAGE_TAGS = {
-    messages.SUCCESS: 'alert alert-success alert-dismissible',
-    messages.ERROR: 'alert alert-danger alert-dismissible',
-}
